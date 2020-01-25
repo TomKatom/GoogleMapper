f3819f2e8a8787ad9a0e883572af799fe9f8109c
Tom Chengich <tomcengich@gmail.com>
2020-01-25 17:39:09 +0200

    Implemented Delete marker

--- Server/routes/markers.js
+++ Server/routes/markers.js
@@ -3,7 +3,20 @@ var router = express.Router();
 var bodyParser = require('body-parser');
 var { authorized } = require('./auth.js');
 const { check, validationResult, body, sanitizeBody } = require('express-validator');
-const {checkExistsMarkerId} = require('./checkExistInDB.js')
+const {checkExistsMarkerId} = require('./checkExistInDB.js');
+
+let checkIfOwnMarker = (id, markerId) => {
+    return new Promise((resolve, reject) => {
+        conn.query(`select * from tblMarkers where userId = ${id} and markerId = ${markerId}`, (err, rows, fields) => {
+            if(rows.length === 1){
+                resolve(rows[0]);
+            }
+            else{
+                reject('You dont own the specified marker.');
+            }
+        });
+    });
+};
 
 router.post('/', authorized, [
     check('name').notEmpty().withMessage("You need to supply a name."),
@@ -38,6 +51,7 @@ router.put('/', authorized, [
     check('latitude').isDecimal().withMessage("You need to supply a decimal value."),
     check('markerId').isNumeric().withMessage("You need to supply a markerId."),
     check('markerId').custom((value) => checkExistsMarkerId(value)),
+    check('markerId').custom((value, {req}) => checkIfOwnMarker(req.session.userId, value)),
     body('name').trim().escape()
 ], (req, res) => {
     const errors = validationResult(req);
@@ -50,6 +64,23 @@ router.put('/', authorized, [
             else res.json({msg: "Marker updated successfully."});
         })
     }
+});
+
+router.delete('/', authorized, [
+    check('markerId').isNumeric().withMessage("You need to supply a markerId."),
+    check('markerId').custom((value) => checkExistsMarkerId(value)),
+    check('markerId').custom((value, {req}) => checkIfOwnMarker(req.session.userId, value)),
+], (req, res) => {
+   const errors = validationResult(req);
+   if(!errors.isEmpty()) {
+       return res.status(422).json({ errors: errors.array() });
+   }
+   else{
+       conn.query(`delete from tblMarkers where markerId = ${req.body.markerId}`, (err, rows, fields) => {
+           if(err) res.status(500).json({msg: "Error deleting marker."});
+           else res.json({msg: "Marker deleted successfully."});
+       });
+   }
 });
 
 
